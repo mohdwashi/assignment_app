a28c91ccbf6e13b12486bcaf332c103aa7a0d247
mod washi <your.email@example.com>
2013-03-12 23:50:14 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssignmentApp::Application.config.secret_token = 'ce93e96a35981079000cc8da48778399ab1d6d308d4f374ea934935fd441402d561482a1c4bf7ac59739db2424d9299c53e04fbfabaccba9afbf808f9ec14dc0'
